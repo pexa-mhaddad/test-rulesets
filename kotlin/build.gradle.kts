692107608f16d0556da9ed8ae64a7563db86dde9
pexa-mhaddad <160090720+pexa-mhaddad@users.noreply.github.com>
2025-05-20 16:48:09 +1000

    Downgrade dependencies: set OkHttp to 4.11.0, Express to 5.1.0, and AWS provider version to 5.1.0

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
 
 application {
